ab2aed4d0e3864f37dc278d93bc05d95017ec7ab
Anna Venediktova <9213998099@mail.ru>
2020-03-22 23:52:16 +0300

    edit html

--- js/openmenu.js
+++ js/openmenu.js
@@ -3,5 +3,19 @@
   const menuBtn = document.querySelector('.hamburger__menu');
   const menuBlock = document.querySelector('menu');
   
+  menuBtn.addEventListener('click', function () {
+  menuBlock.classList.toggle('active');
+  menuBtn.classList.toggle('closed'); 
+    
+  })
+
+  menuBlock.addEventListener('click', function(e) {
+   if (menuBlock.classList.contains('active') && e.target.classList.contains('menu__link')) {
+      menuBlock.classList.toggle('active');
+      menuBtn.classList.toggle('closed'); 
+   }
+    
+  })
+
 }) ();
 
