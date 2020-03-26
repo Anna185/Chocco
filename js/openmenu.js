ceb7bdbe0ae6e5e6ceb95e1f4db1ab9932576cb1
Anna Venediktova <9213998099@mail.ru>
2020-03-26 17:34:19 +0300

    edit js

--- js/openmenu.js
+++ js/openmenu.js
@@ -22,20 +22,59 @@
 (function openMenuChocco() {
 
   const menuList = document.querySelector('.chocco__accordeon-list');
-  const menuBlock = document.querySelector('.chocco__accordeon-item');
-  const menuBtn = document.querySelector('.close-btn');
-  
+  const menuBlock = document.querySelectorAll('.chocco__accordeon-item');
+  const menuBtn = document.querySelectorAll('.close-btn');
+
   menuList.addEventListener('click', function (e) {
+  
   if (e.target.classList.contains ('chocco__accordeon-item-btn')) {
-    menuBlock.classList.toggle('active');
+    const targetBlock = e.target.parentNode;
+    for (let item of menuBlock) {
+      if (item.classList.contains('active') && item !== targetBlock) {
+        item.classList.remove('active');
+        
+      }
+    }
+       targetBlock.classList.toggle('active');
+  }
+    if (e.target.classList.contains ('chocco__accordeon-item-title')) {
+    const targetBlock = e.target.parentNode.parentNode;
+    for (let item of menuBlock) {
+      if (item.classList.contains('active') && item !== targetBlock) {
+        item.classList.remove('active');
+      }
+    }
+       targetBlock.classList.toggle('active');
   }
 })
-
-
-
   }) ();
 
- 
+  
+  
+(function openMenuTeam() {
 
+  const teamList = document.querySelector('.team__list-accordeon');
+  const teamBlock = document.querySelectorAll('.team__item-accordeon');
+  
+
+  teamList.addEventListener('click', function (e) {
+  
+  if (e.target.classList.contains ('team__item-accordeon-title')) {
+    const targetBlock = e.target.parentNode;
+    for (let item of teamBlock) {
+      if (item.classList.contains('active') && item !== targetBlock) {
+        item.classList.remove('active');
+        
+      }
+    }
+       targetBlock.classList.toggle('active');
+  }
+    
+})
+  }) ();
+
+  
+
+  
 
 
