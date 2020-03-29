dceed0da549c8accf7887cedc696d4c4649d4684
Anna Venediktova <9213998099@mail.ru>
2020-03-29 18:58:39 +0300

    edit js

--- js/openmenu.js
+++ js/openmenu.js
@@ -99,7 +99,62 @@ function loop(direction, e) {
 }) ();
 
    
-  
+ const myForm = document.querySelector('#order');
+ const send = document.querySelector('#send');
+
+ send.addEventListener('click', event => {
+    event.preventDefault();
+
+   if (validateForm(myForm)) {
+     const data = {
+       name: myForm.elements.name.value,
+       pnone: myForm.elements.phone.value,
+       comment: myForm.elements.comment.value
+     };
+
+     const xhr = new XMLHttpRequest();
+     xhr.responseType = 'json';
+     xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+     xhr.send(JSON.stringify(data));
+     xhr.addEventListener('load', () => {
+      if(xhr.response.status) {
+        console.log('заказ отправлен');
+      }
+
+     });
+
+
+   }
+ });
+
+ function validateForm(form) {
+   let valid = true;
+
+   if (!validateField(form.elements.name)) {
+     valid = false;
+   }
+
+   if (!validateField(form.elements.phone)) {
+    valid = false;
+  }
+
+  if (!validateField(form.elements.comment)) {
+    valid = false;
+  }
+
+  return valid;
+ }
+
+ function validateField(field) {
+   field.nextElementSibling.textContent = field.validationMessage;
+   return field.checkValidity();
+ }
+
+ 
+
+
+
+
   
   
   
