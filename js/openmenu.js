9b25f83f97f01cbf2017006df371e34b024af83c
Anna Venediktova <9213998099@mail.ru>
2020-03-29 20:14:37 +0300

    edit js

--- js/openmenu.js
+++ js/openmenu.js
@@ -99,33 +99,7 @@ function loop(direction, e) {
 }) ();
 
    
- const myForm = document.querySelector('#order');
- const send = document.querySelector('#send');
-
- send.addEventListener('click', event => {
-    event.preventDefault();
-
-   if (validateForm(myForm)) {
-     const data = {
-       name: myForm.elements.name.value,
-       pnone: myForm.elements.phone.value,
-       comment: myForm.elements.comment.value
-     };
-
-     const xhr = new XMLHttpRequest();
-     xhr.responseType = 'json';
-     xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
-     xhr.send(JSON.stringify(data));
-     xhr.addEventListener('load', () => {
-      if(xhr.response.status) {
-        console.log('заказ отправлен');
-      }
-
-     });
-
-
-   }
- });
+ 
 
  function validateForm(form) {
    let valid = true;
@@ -151,12 +125,51 @@ function loop(direction, e) {
  }
 
  
+ (function Form() {
+let formData = new FormData(document.forms.order);
+let xhr = new XMLHttpRequest();
+xhr.open("POST", "https://webdev-api.loftschool.com/sendmail");
+xhr.send(formData);
+xhr.onload = () => alert(xhr.response);
+}) ();
 
 
+const openButton = document.querySelector("#openOverlay");
+const successOverlay = createOverlay("Заказ отправлен");
 
+openButton.addEventListener("click", function() {
+  document.body.appendChild(successOverlay);
+});
 
-  
-  
-  
-  
-    
+function createOverlay(content) {
+  const overlayElement = document.createElement("div");
+  overlayElement.classList.add("overlay");
+  overlayElement.addEventListener("click", function(e) {
+    if (e.target === overlayElement) {
+      closeElement.click();
+    }
+  });
+
+  const containerElement = document.createElement("div");
+  containerElement.classList.add("container");
+
+  const contentElement = document.createElement("div");
+  contentElement.classList.add("content");
+  contentElement.innerHTML = content;
+
+  const closeElement = document.createElement("a");
+  closeElement.classList.add("close");
+  closeElement.textContent = "x";
+  closeElement.href = "#";
+  closeElement.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.body.removeChild(overlayElement);
+  });
+
+  overlayElement.appendChild(containerElement);
+  containerElement.appendChild(closeElement);
+  containerElement.appendChild(contentElement);
+
+  return overlayElement;
+}
+
