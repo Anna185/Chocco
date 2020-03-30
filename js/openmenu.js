932756dcd60c07fba23001e65156a1d0440eabee
Anna Venediktova <9213998099@mail.ru>
2020-03-30 20:55:24 +0300

    edit js

--- js/openmenu.js
+++ js/openmenu.js
@@ -2,22 +2,22 @@
 (function openMenu() {
   const menuBtn = document.querySelector('.hamburger__menu');
   const menuBlock = document.querySelector('.menu');
-  
+
   menuBtn.addEventListener('click', function () {
-  menuBlock.classList.toggle('active');
-  menuBtn.classList.toggle('closed'); 
-    
+    menuBlock.classList.toggle('active');
+    menuBtn.classList.toggle('closed');
+
   })
 
-  menuBlock.addEventListener('click', function(e) {
-   if (menuBlock.classList.contains('active') && e.target.classList.contains('menu__link')) {
+  menuBlock.addEventListener('click', function (e) {
+    if (menuBlock.classList.contains('active') && e.target.classList.contains('menu__link')) {
       menuBlock.classList.toggle('active');
-      menuBtn.classList.toggle('closed'); 
-   }
-    
+      menuBtn.classList.toggle('closed');
+    }
+
   })
 
-}) ();
+})();
 
 (function openMenuChocco() {
 
@@ -26,150 +26,137 @@
   const menuBtn = document.querySelectorAll('.close-btn');
 
   menuList.addEventListener('click', function (e) {
-  
-  if (e.target.classList.contains ('chocco__accordeon-item-btn')) {
-    const targetBlock = e.target.parentNode;
-    for (let item of menuBlock) {
-      if (item.classList.contains('active') && item !== targetBlock) {
-        item.classList.remove('active');
-        
-      }
-    }
-       targetBlock.classList.toggle('active');
-  }
-    if (e.target.classList.contains ('chocco__accordeon-item-title')) {
-    const targetBlock = e.target.parentNode.parentNode;
-    for (let item of menuBlock) {
-      if (item.classList.contains('active') && item !== targetBlock) {
-        item.classList.remove('active');
-      }
-    }
-       targetBlock.classList.toggle('active');
-  }
-})
-  }) ();
 
-  
-  
+    if (e.target.classList.contains('chocco__accordeon-item-btn')) {
+      const targetBlock = e.target.parentNode;
+      for (let item of menuBlock) {
+        if (item.classList.contains('active') && item !== targetBlock) {
+          item.classList.remove('active');
+
+        }
+      }
+      targetBlock.classList.toggle('active');
+    }
+    if (e.target.classList.contains('chocco__accordeon-item-title')) {
+      const targetBlock = e.target.parentNode.parentNode;
+      for (let item of menuBlock) {
+        if (item.classList.contains('active') && item !== targetBlock) {
+          item.classList.remove('active');
+        }
+      }
+      targetBlock.classList.toggle('active');
+    }
+  })
+})();
+
+
+
 (function openMenuTeam() {
 
   const teamList = document.querySelector('.team__list-accordeon');
   const teamBlock = document.querySelectorAll('.team__item-accordeon');
-  
+
 
   teamList.addEventListener('click', function (e) {
-  
-  if (e.target.classList.contains ('team__item-accordeon-title')) {
-    const targetBlock = e.target.parentNode;
-    for (let item of teamBlock) {
-      if (item.classList.contains('active') && item !== targetBlock) {
-        item.classList.remove('active');
-        
+
+    if (e.target.classList.contains('team__item-accordeon-title')) {
+      const targetBlock = e.target.parentNode;
+      for (let item of teamBlock) {
+        if (item.classList.contains('active') && item !== targetBlock) {
+          item.classList.remove('active');
+
+        }
       }
+      targetBlock.classList.toggle('active');
     }
-       targetBlock.classList.toggle('active');
-  }
-    
-})
-  }) ();
+
+  })
+})();
 
 
-  
+
 (function Slider() {
-const left = document.querySelector("#left");
-const right = document.querySelector("#right");
-const items = document.querySelector("#items");
+  const left = document.querySelector("#left");
+  const right = document.querySelector("#right");
+  const items = document.querySelector("#items");
 
-right.addEventListener("click", function(e) {
- loop("right", e);
+  right.addEventListener("click", function (e) {
+    loop("right", e);
+  });
+
+  left.addEventListener("click", function (e) {
+    loop("left", e);
+  });
+
+  function loop(direction, e) {
+    e.preventDefault();
+    if (direction === "right") {
+      items.appendChild(items.firstElementChild);
+    } else {
+      items.insertBefore(items.lastElementChild, items.firstElementChild);
+    }
+  }
+})();
+
+
+(function SendOrderForm() {
+const orderForm = document.querySelector('#orderForm');
+const submitBtn = document.querySelector('.btn__order');
+
+submitBtn.addEventListener('click', function (event) {
+  if (orderForm.checkValidity()) {
+    event.preventDefault();
+
+    const data = new FormData();
+    data.append("name", orderForm.elements.name.value);
+    data.append("phone", orderForm.elements.phone.value);
+    data.append("comment", orderForm.elements.comment.value);
+    data.append("to", "9213998099@mail.ru");
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail', true);
+    xhr.responseType = 'json';
+    xhr.send(JSON.stringify(data));
+  }
+  
 });
 
-left.addEventListener("click", function(e) {
- loop("left", e);
-});
-
-function loop(direction, e) {
- e.preventDefault();
- if (direction === "right") {
-   items.appendChild(items.firstElementChild);
- } else {
-   items.insertBefore(items.lastElementChild, items.firstElementChild);
- }
-}
-}) ();
-
-   
- 
-
- function validateForm(form) {
-   let valid = true;
-
-   if (!validateField(form.elements.name)) {
-     valid = false;
-   }
-
-   if (!validateField(form.elements.phone)) {
-    valid = false;
-  }
-
-  if (!validateField(form.elements.comment)) {
-    valid = false;
-  }
-
-  return valid;
- }
-
- function validateField(field) {
-   field.nextElementSibling.textContent = field.validationMessage;
-   return field.checkValidity();
- }
-
- 
- (function Form() {
-let formData = new FormData(document.forms.order);
-let xhr = new XMLHttpRequest();
-xhr.open("POST", "https://webdev-api.loftschool.com/sendmail");
-xhr.send(formData);
-xhr.onload = () => alert(xhr.response);
-}) ();
-
-
-const openButton = document.querySelector("#openOverlay");
-const successOverlay = createOverlay("Заказ отправлен");
+(function Overlay() {
+const openButton = document.querySelector(".openOverlay");
+const template = document.querySelector("#overlayTemplate").innerHTML;
+const overlay = createOverlay(template);
 
 openButton.addEventListener("click", function() {
-  document.body.appendChild(successOverlay);
+  overlay.open();
+  overlay.setContent("Спасибо, данные сохранены");
 });
 
-function createOverlay(content) {
-  const overlayElement = document.createElement("div");
-  overlayElement.classList.add("overlay");
-  overlayElement.addEventListener("click", function(e) {
+function createOverlay(template) {
+  const fragment = document.createElement('div');
+
+  fragment.innerHTML = template;
+
+  const overlayElement = fragment.querySelector(".overlay");
+  const contentElement = fragment.querySelector(".content");
+  const closeElement = fragment.querySelector(".close");
+  
+  overlayElement.addEventListener("click", e => {
     if (e.target === overlayElement) {
       closeElement.click();
     }
   });
-
-  const containerElement = document.createElement("div");
-  containerElement.classList.add("container");
-
-  const contentElement = document.createElement("div");
-  contentElement.classList.add("content");
-  contentElement.innerHTML = content;
-
-  const closeElement = document.createElement("a");
-  closeElement.classList.add("close");
-  closeElement.textContent = "x";
-  closeElement.href = "#";
-  closeElement.addEventListener("click", function(e) {
-    e.preventDefault();
+  closeElement.addEventListener("click", () => {
     document.body.removeChild(overlayElement);
   });
 
-  overlayElement.appendChild(containerElement);
-  containerElement.appendChild(closeElement);
-  containerElement.appendChild(contentElement);
-
-  return overlayElement;
-}
-
+  return {
+    open() {
+      document.body.appendChild(overlayElement);
+    },
+    close() {
+      closeElement.click();
+    },
+    setContent(content) {
+      contentElement.innerHTML = content;
+    }
+  };
+}); 
