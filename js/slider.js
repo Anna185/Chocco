eed205f6e41a27c35d74e55ace85c02f44c274f0
Anna Venediktova <9213998099@mail.ru>
2020-04-04 18:54:39 +0300

    edit js

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,22 @@
-$(function () {
+(function Slider() {
+  const left = document.querySelector("#left");
+  const right = document.querySelector("#right");
+  const items = document.querySelector("#items");
 
-  $('.arrow--right').on('click', function(e){
-    e.preventDefault();
-
-    var $this = $(this),
-    container = $this.closest('.bars__list'),
-    
-
+right.addEventListener("click", function (e) {
+    loop("right", e);
   });
 
+  left.addEventListener("click", function (e) {
+    loop("left", e);
+  });
 
-});
+  function loop(direction, e) {
+   e.preventDefault();
+    if (direction === "right") {
+      items.appendChild(items.firstElementChild);
+    } else {
+     items.insertBefore(items.lastElementChild, items.firstElementChild);
+    }
+  }
+})();
